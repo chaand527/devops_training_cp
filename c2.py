6e5f12027e94ff64a2280b06885f6b58eec86041
chandru <chaand527@gmail.com>
2025-09-12 17:47:07 +0000

    2nd python code

---
+++ c2.py
@@ -0,0 +1,21 @@
+import time,sys
+
+
+indent = 0
+indent_increasing = True
+try:
+    while True:
+        print(' '*indent, end= '')
+        print('*'*8)
+        time.sleep(.1)
+        if indent_increasing:
+            indent += 1
+            if indent == 20:
+                indent_increasing = False
+        else:
+            indent -= 1
+            if indent == 0:
+                indent_increasing = True
+except KeyboardInterrupt:
+    sys.exit()
+                    
